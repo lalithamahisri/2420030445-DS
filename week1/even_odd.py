690e058274d111d2b120a5f0d7acc61bb203a819
lalithamahisri <lalithamahisri06@gmail.com>
2026-09-22 12:03:14 +0530

    Delete week1 directory

--- week1/even_odd.py
+++
@@ -1,8 +0,0 @@
-def even_odd(n) :
-    if n%2 ==0:
-        print(f"{n} is an even number")
-    else:
-        print(f"{n} is an odd number")
-
-n = int(input())
-even_odd(n)
--- week1/function.py
+++
@@ -1,6 +0,0 @@
-def find_square(n):
-    return n*n
-
-num = input()
-sqr = find_square(int(num))
-print(f"The square of {num} is {sqr}")
