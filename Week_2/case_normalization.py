e37d5958f838fc8f90884b7140f7d3dab6d49b90
lalithamahisri <lalithamahisri06@gmail.com>
2026-09-22 12:02:26 +0530

    Delete Week_2 directory

--- Week_2/case_normalization.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-df = pd.DataFrame({
-    'Name':['Alice','Bob','charles','DAVID']
-})
-
-#to lowercase
-df['Name_lower'] = df['Name'].str.lower()
-df['Name_upper'] = df['Name'].str.upper()
-print(df)
